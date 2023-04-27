14274945ed2cf8b682ae7bac06edaab7c7936cdb
Yue Heng <wongyueheng@gmail.com>
2023-04-27 14:55:26 +0800

    added analytics fragment, refactored some code

--- app/src/main/java/com/glyph/oskihealth/MainActivity.kt
+++ app/src/main/java/com/glyph/oskihealth/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.chatFragment, R.id.analyticsFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigation.setupWithNavController(navController)
     }
