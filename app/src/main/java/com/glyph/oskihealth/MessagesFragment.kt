3cc7655848a8937a672bb76ce16160877613c0ab
Mingy123 <mingy123.coc@gmail.com>
2023-04-27 22:59:12 +0800

    networking: eventstream done

--- app/src/main/java/com/glyph/oskihealth/MessagesFragment.kt
+++ app/src/main/java/com/glyph/oskihealth/MessagesFragment.kt
@@ -1,6 +1,7 @@
 package com.glyph.oskihealth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +16,13 @@ import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputEditText
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.HttpURLConnection
+import java.net.SocketException
+import java.net.URL
 import java.util.Calendar
 import java.util.HashMap
 
@@ -26,6 +34,11 @@ class MessagesFragment : Fragment() {
     private var columnCount = 1
     private lateinit var queue: RequestQueue
     private lateinit var person: String
+    private lateinit var connection: HttpURLConnection
+
+    companion object {
+        val connections = HashMap<String, HttpURLConnection>()
+    }
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
@@ -73,7 +86,7 @@ class MessagesFragment : Fragment() {
                 content.text?.clear()
 
                 val sendRequest = object : AuthorisedRequest(Method.POST, "/send", {}, {}) {
-                    override fun getParams(): MutableMap<String, String>? {
+                    override fun getParams(): MutableMap<String, String> {
                         val old = super.getParams()
                         val new = HashMap<String, String>()
                         if (old != null) for ((key, value) in old) new[key] = value
@@ -87,6 +100,51 @@ class MessagesFragment : Fragment() {
 
             content.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
                 recyclerView.scrollToPosition(messages.size-1)
+            }
+
+
+            // event stream yes
+            val coroutineScope = CoroutineScope(Dispatchers.IO)
+            connections[person]?.disconnect()
+            coroutineScope.launch {
+                val url = URL(AuthorisedRequest.HOST + "/subscribe")
+                connection = withContext(Dispatchers.IO) { url.openConnection() } as HttpURLConnection
+                connections[person] = connection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Accept", "text/event-stream")
+                connection.setRequestProperty("X-Username", AuthorisedRequest.USERNAME)
+                connection.setRequestProperty("X-Password", AuthorisedRequest.PASSWORD)
+
+                val postData = "username=$person".toByteArray(Charsets.UTF_8)
+                connection.setRequestProperty("Content-Length", postData.size.toString())
+                connection.doOutput = true
+                val outputStream = connection.outputStream
+                withContext(Dispatchers.IO) { outputStream.write(postData) }
+                withContext(Dispatchers.IO) { outputStream.flush() }
+
+                val reader = connection.inputStream.bufferedReader(Charsets.UTF_8)
+
+                try {
+                    while (true) {
+                        val line = withContext(Dispatchers.IO) { reader.readLine() }
+                        for (i in 0..5) Log.d("mingy", line)
+                        if (line == null) break
+                        else if (line.isEmpty()) continue
+                        val dataField = Regex("^data:(.*)$").find(line)?.groupValues?.get(1)
+                        if (dataField != null) {
+                            val message = Gson().fromJson(dataField, Message::class.java)
+                            for (i in 0..5) Log.d("mingy", message.content)
+                            if (message.sender == AuthorisedRequest.USERNAME) continue
+                            withContext(Dispatchers.Main) {
+                                messages.add(message)
+                                recyclerView.adapter?.notifyItemInserted(messages.size - 1)
+                                recyclerView.scrollToPosition(messages.size - 1)
+                            }
+                        }
+                    }
+                } catch (_: SocketException) {
+                    for (i in 0..100) Log.d("mingy", "eventstream closed")
+                } // cry about it
             }
         }
         return view
