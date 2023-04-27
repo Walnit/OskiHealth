1b9fea7850fa6a8e3b85bbd1b95863f3039e9a12
Yue Heng <wongyueheng@gmail.com>
2023-04-27 21:57:15 +0800

    added analytics via vico library

--- app/src/main/java/com/glyph/oskihealth/AnalyticsFragment.kt
+++ app/src/main/java/com/glyph/oskihealth/AnalyticsFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ToggleButton
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.button.MaterialButtonToggleGroup
+import com.patrykandpatrick.vico.core.entry.entryModelOf
+import com.patrykandpatrick.vico.views.chart.ChartView
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +23,34 @@ class AnalyticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analytics, container, false)
+        val view = inflater.inflate(R.layout.fragment_analytics, container, false)
+        with (view) {
+            val trendChartEntryModel = entryModelOf(-5f, -4.6f, -2.3f, 0f, 1.3f, 4.2f, 5f)
+            val trendChart: ChartView = findViewById(R.id.chart_view_trend)
+            trendChart.setModel(trendChartEntryModel)
+            val trendLayout: ConstraintLayout = findViewById(R.id.trend_layout)
+
+
+            val valueChartEntryModel = entryModelOf(4f, 12f, 8f, 16f)
+            val valueChart: ChartView = findViewById(R.id.chart_view_value)
+            valueChart.setModel(valueChartEntryModel)
+            val valueLayout: ConstraintLayout = findViewById(R.id.value_layout)
+
+            val toggleButton: MaterialButtonToggleGroup = findViewById(R.id.toggleButton)
+            toggleButton.addOnButtonCheckedListener { _, checkedId, isChecked ->
+                // Respond to button selection
+                if (isChecked) {
+                    if (checkedId == R.id.trend_button) {
+                        trendLayout.visibility = View.VISIBLE
+                        valueLayout.visibility = View.GONE
+                    } else if (checkedId == R.id.values_button) {
+                        trendLayout.visibility = View.GONE
+                        valueLayout.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+
+        return view
     }
 }
